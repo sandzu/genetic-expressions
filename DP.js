61517ea0ff7fdfb2a6c798d6483c771e2b6bfa42
Andzu Schaefer <countermatter7@gmail.com>
2018-09-26 21:17:20 -0700

    DP solution first commit

---
+++ DP.js
@@ -0,0 +1,76 @@
+
+const OPERATIONS = ['+', '-', '*', '/',];
+const NUMBERS = '123456789'.split('');
+
+//every iteration, we store expressions and results in hashes in the form {result: [expression1, expression2...]]}
+const outcomes = (a,b) => {
+    const res = {};
+    OPERATIONS.forEach(op => {
+        res[eval( a + op + b )] = a + op + b;
+    });
+    return res;
+}
+
+class myHash {
+    constructor(){
+        this.store = {};
+    }
+
+    keys(){
+        return Object.keys(this.store);
+    }
+
+    values(){
+        return Object.values(this.store);
+    }
+
+    set(k,v){
+        if(this.store[k] === undefined){
+            this.store[k] = [];
+        }
+        this.store[k].push(v);
+    }
+
+    get(k){
+        return this.store[k];
+    }
+
+    merge(h){
+        h.keys.forEach(k => {
+            this.set(k, h.get(k));
+        })
+        return this;
+    }
+}
+
+
+//hardcode the first result
+const first_case = new myHash;
+console.log(first_case);
+first_case.set(NUMBERS[0], NUMBERS.shift());
+console.log(first_case);
+var computed = first_case;
+
+NUMBERS.forEach(number => {
+    let all_results = new myHash;
+    computed.values().forEach( previous_result_array => {
+        previous_result_array.forEach( previous_result => {
+            resultsMappedToExpressions = outcomes(previous_result, number);
+            Object.keys(resultsMappedToExpressions).forEach(outcome => {
+                all_results.set(outcome, resultsMappedToExpressions[outcome]);   
+            });
+        });
+    });
+    computed = all_results;
+});
+
+
+
+console.log(computed.get('100'));
+
+//just to be sure
+computed.get('100').forEach( expression => {
+    if(eval(expression) !== 100){
+        console.log('ERR ', expression, " EVALUATES TO ", eval(expression));
+    }
+});
